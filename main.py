e51586fd29db2ab986704f617c85cff6ea6971af
Brennan Thompson <b.m.technologies@outlook.com>
2024-12-12 17:01:43 -0500

    This program showcases the input method.  I took it a step further giving the user an option to continue printing to the console.  Error checking makes sure that a Y and or an N is given by the user and if not, to prompt the user to input a Y or an N.
    I learned that you can change the input given by the user directly on the input path by using .upper() at the end of the input method.  You can change the case directly with the input method.  Also have practice with boolean variables of changing from true to false.  Used a number of while loops and if checks.

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 def main():
-    print("This program is to showcase my devleopment with Python")
-    print("Hello, World! The show must go on!")
+
+    endProgram = False
+
+
+    while endProgram == False:
+
+        #Asking the user to input a desired printout and then prints it out
+        userInput = input ("Please enter the string you want to be printed out: ")
+        print(userInput)
+
+        #Asking the user if they want to print another input suggestion. Changes all to uppercase for program sesitivity.
+        userDecision = input ("Enter Y to do again.  Enter N to end program: ").upper()
+
+        #Check case.  If any case isn't true...triggers the user to try again
+        while userDecision != "Y" and userDecision != "N":
+            userDecision = input("Sorry, you must enter Y or N. Do you want to continue printing data to the console? ").upper()
+
+
+            if userDecision == "Y" or userDecision == "N":
+                break
+
+        if userDecision == "N":
+
+            endProgram = True
+
 
 if __name__ == "__main__":
     main()
+    print("Goodbye!")
